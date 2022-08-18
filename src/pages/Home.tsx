7c5e74e2e4878417bd00be0e56d2488660037d00
Damian Tarnawsky <damian@ionic.io>
2022-08-18 11:25:50 -0700

    (feat) expose keyboard access issue with fabbutton slot fixed

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { IonContent, IonFab, IonFabButton, IonIcon, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonList, IonItem, IonFab, IonFabButton, IonIcon, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import { camera } from 'ionicons/icons';
 import './Home.css';
@@ -17,12 +17,17 @@ const Home: React.FC = () => {
             <IonTitle size="large">Blank</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer />
+        {/* <ExploreContainer /> */}
         <IonFab aria-label={'new diary'} vertical='bottom' horizontal='end' slot='fixed'>
           <IonFabButton aria-label={'new diary'} routerLink="https://google.com">
             <IonIcon aria-label={'new diary'} icon={camera} />
           </IonFabButton>
         </IonFab>
+        <IonList>
+          <IonItem button>Item 1</IonItem>
+          <IonItem button>Item 2</IonItem>
+          <IonItem button>Item 3</IonItem>
+        </IonList>
       </IonContent>
     </IonPage>
   );
